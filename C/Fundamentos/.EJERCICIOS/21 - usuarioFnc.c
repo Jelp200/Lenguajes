f3fb461c36866f0960c6d158ecca685cd6275021
Jelp200 <jorge.pena0516@gmail.com>
2022-11-07 18:33:50 -0600

    :croissant: [ADD] PROGRAMAS

---
+++ C/Fundamentos/.EJERCICIOS/21 - usuarioFnc.c
@@ -0,0 +1,25 @@
+/*
+** #########################################################################################
+**  Archivo         :   21 - usuarioFnc.c
+**  Proyecto        :   Lenaguajes
+**  Herramienta(s)  :   Code::Blocks / Visual Studio Code
+**  Compilador      :   C/C++ Compiler (Windows)
+**  Autores         :
+**                      Jelp 200
+**  Descripcion     :
+**                      Realizar un programa que visualize la siguiente
+**                      informacion en consola a travez de firentes funciones.
+**                      [Usuario] [Edad] [Pais]
+** #########################################################################################
+*/
+
+// Directivas de preprocesador.
+#include <stdio.h>
+#include <stdlib.h>
+
+// Funcion principal.
+int main(void){
+
+    printf("\n\t[Jelp 200] [22] [Mexico]");
+    return 0;
+}
