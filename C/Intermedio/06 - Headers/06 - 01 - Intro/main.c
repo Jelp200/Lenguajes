ab6828c2963fff2d0c715ea0e68a5ba2d0d7adac
Jelp200 <jorge.pena0516@gmail.com>
2022-11-16 22:57:39 -0600

    :croissant: [ADD] PROGRAMAS

---
+++ C/Intermedio/06 - Headers/06 - 01 - Intro/main.c
@@ -0,0 +1,45 @@
+/*
+** #########################################################################################
+**      Archivo: main.c
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**                   Code::Blocks
+** #########################################################################################
+*/
+
+// Directivas de prerpocesador
+#include <stdio.h>
+//! Llamamos a las librerias creadas
+#include "suma.h"
+#include "resta.h"
+
+// Main Function
+int main() {
+
+    //! Creamos un apuntador para trabajar con las librerias.
+    int *p_a, *p_b;
+    int a = 1, b = 3;
+
+    p_a = &a;
+    p_b = &b;
+
+    //! Llamamos a la función dentro del archivo .h
+    int RS = suma(p_a, p_b);
+    printf("\n\tLa suma es: %i", RS);
+
+    wit();
+
+    int RR = resta(p_a, p_b);
+    printf("\n\tLa resta es: %i", RR);
+
+    wit();
+    
+    return 0;
+}
+
+// Funciones.
+void wit(){
+    puts("\n");
+    system("PAUSE");
+    system("CLS");
+}
