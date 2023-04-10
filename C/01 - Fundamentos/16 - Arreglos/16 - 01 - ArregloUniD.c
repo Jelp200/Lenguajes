dd03a0f8cf0617dacf295d29096cec2d5506b796
Jelp200 <jorge.pena0516@gmail.com>
2023-04-09 22:30:54 -0600

    :croissant: [MOD] PROGRAMAS

--- C/01 - Fundamentos/16 - Arreglos/16 - 01 - ArregloUniD.c
+++ C/01 - Fundamentos/16 - Arreglos/16 - 01 - ArregloUniD.c
@@ -9,7 +9,6 @@
 
 // Directivas de prerpocesador
 #include <stdio.h>
-#include <math.h>
 
 // Main Function
 int main(){
@@ -18,32 +17,34 @@ int main(){
     int arregloUniD[3] = {24, 05, 93};                                      // Declaramos el arreglo.
     
     // Visualizamos los valor del arreglo en cada posición.
-    printf("El primer valor del arreglo es: %i", arregloUniD[0]);
-    printf("\nEl segundo valor del arreglo es: %i", arregloUniD[1]);
-    printf("\nEl tercer valor del arreglo es: %i", arregloUniD[2]);
+    printf("\n\tEl primer valor del arreglo es: %i", arregloUniD[0]);
+    printf("\n\tEl segundo valor del arreglo es: %i", arregloUniD[1]);
+    printf("\n\tEl tercer valor del arreglo es: %i", arregloUniD[2]);
 
     // Ahora veremos las dos principales formas de manipulación de un arreglo.
 
     // 1er forma.
-    printf("\n\nDe el nuevo del indice 0: "); scanf("%i", &n);
+    printf("\n\n\tDe el nuevo valor del indice 0: "); scanf("%i", &n);
     arregloUniD[0] = n;
-    printf("El nuevo valor del indice 0 es: %i", arregloUniD[0]);
+    printf("\tEl nuevo valor del indice 0 es: %i", arregloUniD[0]);
 
     // 2da forma.
     arregloUniD[1] = 69;
-    printf("El nuevo valor del indice 1 es: %i", arregloUniD[1]);
+    printf("\n\n\tEl nuevo valor del indice 1 es: %i", arregloUniD[1]);
+
+    puts("\n\n");
 
     // Ingresar datos a un arreglo.
     int arregloUniD2[10];
 
     for (int i = 0; i < 10; i++){
-        printf("Escriba el valor para el indice %i: ", i); scanf("%i", &arregloUniD2[i]);
+        printf("\tEscriba el valor para el indice %i: ", i); scanf("%i", &arregloUniD2[i]);
     }
     
     // Mostraremos los valores del arreglo.
     int indice = -1;
     while (++indice < 10){
-        printf("\nEl valor del array[%i] es: %i", indice, arregloUniD2[indice]);
+        printf("\n\tEl valor del array[%i] es: %i", indice, arregloUniD2[indice]);
     }
     return 0;
 }
