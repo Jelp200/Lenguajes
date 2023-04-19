5f32e071319eaef1d4ba6ebc839422642c661923
Jelp200 <jorge.pena0516@gmail.com>
2023-04-18 23:00:18 -0600

    :snake: [ADD] PROGRAMAS

---
+++ Python/01 - Fundamentos/00 - EstructuraGeneral.py
@@ -0,0 +1,19 @@
+"""
+** #########################################################################################
+**      Archivo: 00-EstructuraGeneral.py
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**                   Code::Blocks
+**      Notas:
+**          En este archivo veremos la estructura general de un programa en Python.
+** #########################################################################################
+"""
+
+# Funcion principal.
+print("Cadena de texto")
+
+"""
+Notas:
+    A diferencia de otros lenguajes de programación en PY no tenemos la necesidad de utilizar
+    el ';'.
+"""
