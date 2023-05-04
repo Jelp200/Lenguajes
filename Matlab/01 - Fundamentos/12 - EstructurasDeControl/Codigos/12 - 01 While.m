1c8dc05be32ca1652b4782731442fa8dbbbb57bf
Jelp200 <jorge.pena0516@gmail.com>
2023-05-04 14:18:33 -0600

    :radioactive: [MOD] PROGRAMAS

--- Matlab/01 - Fundamentos/12 - EstructurasDeControl/Codigos/12 - 01 While.m
+++ Matlab/01 - Fundamentos/12 - EstructurasDeControl/Codigos/12 - 01 While.m
@@ -13,3 +13,26 @@
 **          end
 ** #################################################################################################################
 %}
+
+S = 0;
+
+while (S ~= 10)
+    clc
+
+    A = input('De un numero: ');
+    B = input('De un numero: ');
+
+    S = A + B;
+
+    SS = num2str(S);
+
+    if (S ~= 10)
+        disp(['La suma es: ' SS])
+        seguir = input('');
+        waitfor(seguir)
+    else
+        disp(['La suma es: ' SS ', adios.'])
+        seguir = input('');
+        waitfor(seguir)
+    end
+end
