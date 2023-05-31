b420ff07ae0a7f99e86afb7a094210e522d8db95
Jelp200 <jorge.pena0516@gmail.com>
2023-05-30 23:19:17 -0600

    :radioactive: [ADD] EJERCICIO

---
+++ Matlab/01 - Fundamentos/.EJERCICIOS/00 - DeclaracionDeVariables/00 - 01 - Escalares.m
@@ -0,0 +1,72 @@
+%{
+** ######################################################################################################
+**      Archivo: 00 - 01 - Esca.ares.m
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**                   MATLAB R2023a
+**
+**      Notas:
+**          Realizar las operaciones básicas con los siguientes numeros:
+**              1 & 2; 10 & 30; 50 & 2;
+** ######################################################################################################
+%}
+
+% PRIMER PAR
+a = 1;
+b = 2;
+
+% Operaciones
+S = a + b;
+R = a - b;
+P = a * b;
+C = a / b;
+
+% Imprimimos resultados
+S
+R
+P
+C
+
+cont = input('Presione enter para continuar...');
+waitfor(cont)
+
+clear
+clc
+
+% SEGUNDO PAR
+a = 10;
+b = 30;
+
+% Operaciones
+S = a + b;
+R = a - b;
+P = a * b;
+C = a / b;
+
+% Imprimimos resultados
+S
+R
+P
+C
+
+cont = input('Presione enter para continuar...');
+waitfor(cont)
+
+clear
+clc
+
+% TERCER PAR
+a = 50;
+b = 2;
+
+% Operaciones
+S = a + b;
+R = a - b;
+P = a * b;
+C = a / b;
+
+% Imprimimos resultados
+S
+R
+P
+C
