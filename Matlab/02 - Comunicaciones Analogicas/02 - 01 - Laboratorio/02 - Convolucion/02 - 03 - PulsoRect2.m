9fe69cfb42f892e7def5a9cd4f717a76ac0e4e6b
Jelp200 <jorge.pena0516@gmail.com>
2023-06-20 14:19:44 -0600

    :radioactive: [ADD] PRACTICAS

---
+++ Matlab/02 - Comunicaciones Analogicas/02 - 01 - Laboratorio/02 - Convolucion/02 - 03 - PulsoRect2.m
@@ -0,0 +1,33 @@
+%{
+** ######################################################################################################
+**      Archivo: 02 - 02 - PulsosRect2.m
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**                   MATLAB R2023a
+**
+**      Notas:
+**          En esta parte de la practica se realizara la convolución entre dos pulsos rectangulares con
+**          la funcion pre establecida por matlab llamada "conv".
+** ######################################################################################################
+%}
+
+clear all
+clf                                     % Elimina las figuras
+clc
+
+% MAIN PROGRAM
+N = 4;
+
+% Incrementos de la función
+dt = 1 / N;
+t = 0 : dt : 1;
+x = (1 / 2) * ones(size(t));
+h = ones(size(t));
+y = conv(x, h) * dt;
+t1 = 0 : dt : 2;
+
+plot(t1, y)
+grid on
+axis([0 2 .1 .63])
+grid on
+title('Convolución de pulsos cuadrados con conv')
