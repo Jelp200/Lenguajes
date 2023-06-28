87e86b0f14132ac8537314165e799fbbbe1f9efc
Jelp200 <jorge.pena0516@gmail.com>
2023-06-27 19:54:50 -0600

    :radioactive: [ADD] PRACTICAS

---
+++ Matlab/02 - Comunicaciones Analogicas/02 - 01 - Laboratorio/03 - AM/03 - 05 - DBLPSdomF.m
@@ -0,0 +1,35 @@
+%{
+** ######################################################################################################
+**      Archivo: 03 - 05 - DBLPSdomF.m
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**                   MATLAB R2023a
+**
+**      Notas:
+**          En esta parte de la practica se realizara la T.F para así poder obtener la modulación de
+**          DBLPS en el domino de la frecuencia.
+** ######################################################################################################
+%}
+
+clear all
+clf                                     % Elimina las figuras
+clc
+
+% MAIN PROGRAM
+Fs = 128;                               % Declaramos las cntes
+T = 1 / Fs;
+A = 5;
+fport = 13;
+Amen = 2;
+fmen = 1;
+
+t = 0 : T : 2;                          % Definimos el intervalo
+
+Sport = A * cos(2 * pi* fport * t);     % Declaramos las funciónes
+SPORT = (1 / Fs) .* real(fft(Sport, Fs));
+F = 0 : (Fs / 2) - 1;
+stem(F, SPORT(F + 1), 'r')              % Graficamos los datos discretos
+title('Espectro señal portadora')
+xlabel('f')
+ylabel('F(w)')
+grid on
---
+++ Matlab/02 - Comunicaciones Analogicas/02 - 01 - Laboratorio/03 - AM/Objetivo.md
@@ -0,0 +1,11 @@
+# CONVOLUCIÓN :plus:
+
+- Aprender a utilizar los siguientes comandos en matlab y sus aplicaciones.
+
+'''matlab
+butter
+filter
+'''
+
+- Mostrar en tiempo y frecuencia las señales moduladas de DBLPS AM comercial o sistema con portadora de alta
+potencia banda lateral única.
