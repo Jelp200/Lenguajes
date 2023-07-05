9550f310e1a70d3edc04d15bce13870f943466ab
Jelp200 <jorge.pena0516@gmail.com>
2023-07-05 17:04:00 -0600

    :snake: [ADD] PROGRAMAS

---
+++ Python/01 - Fundamentos/08 - EstructurasCondicionales/Codigos/08 - 05 - OperadorTernario.py
@@ -0,0 +1,25 @@
+"""
+** #########################################################################################
+**      Archivo: 08 - 05 - IfElseIf.py
+**      SO: Windows 10
+**      Herramienta: Visual Studio Code
+**      Notas:
+**         [Expresión 1] if (Condición) else [Expresión 2]
+** #########################################################################################
+"""
+# °°°°°°°°°°°°°°°°°°°°°°°°°°°° F U N C I O N  P R I N C I P A L °°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+n1 = 0; n2 = 0
+
+n1 = int(input("De el primer número: ")); n2 = int(input("De el segundo número: "))
+
+Condicion = (n1 > n2)
+
+print(f'{n1} > {n2}') if Condicion else print(f'{n1} < {n2}')
+# °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+
+""" °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°° S A L I D A °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
+De el primer número: [Numero a dar]
+De el segundo número: [Numero a dar]
+
+[1er numero dado] >/< [2do numero dado]
+"""
