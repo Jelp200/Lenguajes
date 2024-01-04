da199c1834db408d5827effaf9dd68e422f4445a
Jelp200 <jorge.pena0516@gmail.com>
2024-01-04 17:21:51 -0600

    :page_with_curl: [MOD] LEEME.md

--- 08 - uControladores/08 - 01 - BLUEPILL/01 - Plantilla.c
+++ 08 - uControladores/08 - 01 - BLUEPILL/01 - Plantilla.c
@@ -1,36 +1,10 @@
-# BLUEPILL :large_blue_circle:
-<div>
-    <img src="/IMGS/uControladores/STM32/BP.png">
-</div>
-La BluePill es un uControlador con diferentes usos, puede ser utilizada para dar soluciones a aplicaciones simples como apagar y encender un led, o
-tambien puede ser utilizada para procesamiento digital de señales, en esta carpeta veremos sus conceptos básicos, cómo leer su datasheet y como poder
-programar nuestro uControlador, veremos desde aplicaciones con C, hasta aplicaciones con C++.
-<ul>
-    <li><a href="00 - ConceptosBasicos.md">Conceptos básicos</a></li>
-    <li><a href="#">Aplicaciones con C</a></li>
-    <li><a href="#">Aplicaciones con C++</a></li>
-</ul>
-
-### STM32F103 PINOUT
-<img src="01 - 00 - Extras/PINOUT_DIAGRAMA.png">
-
-### STM32F103 DOCUMENTACIÓN IMPORTANTE
-<ul>
-    <li><a href="01 - 00 - Extras/stm32f103c8.pdf">Hoja de datos</a></li>
-    <li><a href="01 - 00 - Extras/STM32F103xx ManualDeReferancia.pdf">Manual de referencia</a></li>
-</ul>
-
-### PLANTILLA
-Al igual que en cualquier otro lenguaje de programación o entorno de desarrollo, contaremos con una plantilla la cual nos permitira estructurar de
-una mejor manera nuestro código, esto es muy recomendable para sistemas embebidos debido a que alginas veces los códigos pueden ser muy extensos.
-```C
 /*
 ** #########################################################################################
 **  Archivo         :   [nombreDelArchivo.c / nombreDelArchivo.cpp]
 **  Proyecto        :   [nombreDelProyecto]
 **  Herramienta(s)  :   [Herramientas utilizadas]
 **  Autores         :
-**                      Peña Paz Jorge Eduardo (Jelp200)
+**                      [Tu nombre o nombres de los integrantes del equipo]
 **  Descripcion     :
 **                      Aquí va la descripción del programa o aplicación que se esta o se
 **                      realizara.
@@ -78,5 +52,3 @@ void tusPrototiposDeFunciones(void){
 Este espacio esta dedicado para realizar notas o aclarar algunos puntos de la
 aplicación o solucion a realizar.
 */
-
-```
