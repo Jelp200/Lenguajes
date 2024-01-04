75ab27f73a97b3c01eb1661f2120d903b8373868
Jelp200 <jorge.pena0516@gmail.com>
2024-01-04 16:57:08 -0600

    :file_folder: [ADD] CARPETAS

--- 04 - Python/04 - Redes/04 - 01 - scapy.py
+++ 04 - Python/04 - Redes/04 - 01 - scapy.py
@@ -1,14 +1,14 @@
 from scapy.all import IP, TCP, Raw, send
 
 # Dirección IP de tu propia máquina
-target_ip = "192.168.8.187"
+target_ip = "192.168.1.87"
 
 # Puerto de origen y destino
 src_port = 12345
 dst_port = 80  # Puedes cambiarlo al puerto que desees
 
 # Mensaje personalizado
-custom_message = "ESIME Zacatenco | IPN | ICE"
+custom_message = "PRUEBA CON EXITO"
 
 # Crea un paquete TCP con la carga útil personalizada
 packet = IP(dst=target_ip) / TCP(sport=src_port, dport=dst_port, flags="S", seq=1000) / Raw(load=custom_message)
